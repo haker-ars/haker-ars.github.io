837e55c69806b42e9c505c6bf0867e4b286504be
haker-ars <89784634+haker-ars@users.noreply.github.com>
2021-09-17 15:36:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const dino = document.getElementById('ninja')
+const cactus = document.getElementById('suriken')
+
+document.addEventListener('keydown', function(event) {
+    jump();
+});
+
+function jump () {
+    if (dino.classList != 'jump') {
+        dino.classList.add('jump')
+    }
+    setTimeout( function() {
+        dino.classList.remove('jump')
+    }, 300)
+}
+let GameOverFalse = setInterval( function() {
+    let dinoPlay = parseInt(window.getComputedStyle(dino).getPropertyValue('top'))
+    let cactusKill = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'))
+
+    if (cactusKill < 50 && cactusKill > 0 && dinoPlay >= 140) {
+        alert('You Ninja Are Killed!!')
+    }
+}, 10)
