22d15244b4263671682a9c9004d9cbd280c723e1
haker-ars <89784634+haker-ars@users.noreply.github.com>
2021-09-18 18:31:04 +0300

    Create script-en.js

---
+++ script-en.js
@@ -0,0 +1,25 @@
+const count = document.getElementById('count');
+const ninja = document.getElementById('ninja');
+const suriken = document.getElementById('suriken');
+
+document.addEventListener('keydown', function(event) {
+    jump();
+});
+
+function jump () {
+    if (ninja.classList != 'jump') {
+        ninja.classList.add('jump')
+    }
+    setTimeout( function() {
+        ninja.classList.remove('jump')
+    }, 300)
+}
+let GameOverFalse = setInterval( function() {
+    let ninjaPlay = parseInt(window.getComputedStyle(ninja).getPropertyValue('top'))
+    let surikenKill = parseInt(window.getComputedStyle(suriken).getPropertyValue('left'))
+
+    if (surikenKill < 50 && surikenKill > 0 && ninjaPlay >= 140) {
+        alert('Your Ninja Was Killed!!');
+        count.innerHTML++;
+    }
+}, 10)
